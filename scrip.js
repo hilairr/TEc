c1c535cfeb664e5a382a9618fc87e39ad87416e5
hilairr <hilairebonkoungou67@gmail.com>
2025-04-16 10:25:29 +0000

    2e commit

---
+++ scrip.js
@@ -0,0 +1,68 @@
+function agrandirImage(){
+    const img = document.getElementById("img1");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img1");
+    img.style.width ="auto";
+}
+
+//img2
+function agrandirImage(){
+    const img = document.getElementById("img2");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img2");
+    img.style.width ="auto";
+}
+ //img3
+ function agrandirImage(){
+    const img = document.getElementById("img3");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img3");
+    img.style.width ="auto";
+}
+//img4
+function agrandirImage(){
+    const img = document.getElementById("img4");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img4");
+    img.style.width ="auto";
+}
+
+//img5
+function agrandirImage(){
+    const img = document.getElementById("img5");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img5");
+    img.style.width ="auto";
+}
+
+//img6
+function agrandirImage(){
+    const img = document.getElementById("img6");
+    img.style.width ="400px";
+    img.style.transition = "width 0.5s ease";
+
+}
+function reduireImage(){
+    const img = document.getElementById("img6");
+    img.style.width ="auto";
+}
